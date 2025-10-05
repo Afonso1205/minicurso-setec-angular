f7da1866ed592ca6166c06e762239b423d323176
Afonso1205 <afonso.ribes@hotmail.com>
2025-10-05 01:39:30 -0300

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,17 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TaskListComponent } from './components/task-list/task-list.component';
 import { TaskService } from './services/task.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, TaskListComponent, NgbModule],
+  imports: [FormsModule, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   newTask = '';
-  taskAdded = false;
 
   constructor(private taskService: TaskService) {}
 
@@ -21,7 +19,6 @@ export class AppComponent {
     if (this.newTask.trim()) {
       this.taskService.addTask(this.newTask);
       this.newTask = '';
-      this.taskAdded = true;
     }
   }
 }
